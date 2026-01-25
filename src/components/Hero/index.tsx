088b2fd26c94041ffd814911fcf2310d7de0585b
Evandro Passaia <evandrobaraka@gmail.com>
2026-01-25 00:58:36 -0300

    feat: adiciona componente de Categorias

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -24,7 +24,7 @@ export const Hero = () => {
                             <Button variant="secondary" size="sm">
                                 Ver modelos
                             </Button>
-                            <Button>
+                            <Button variant="primary" size="md">
                                 Comprar
                             </Button>
                         </div>
